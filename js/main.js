7904b3e06255ebb0f843e2da9e74ca7840d70799
sidhu-sukhbhag <156356427+sidhu-sukhbhag@users.noreply.github.com>
2024-08-17 10:57:15 -0400

    Changes in pages
    Commint changes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-// Get the hamburger menu button and the menu
- const hamburger = document.querySelector('.navbar-toggler');
-const menu = document.querySelector('.innerpage_navbar');
-            
-// Add an event listener to the hamburger menu button
-hamburger.addEventListener('click', () => {
-// Toggle the menu
- menu.classList.toggle('show');
- });
+document.addEventListener('DOMContentLoaded', function() {
+    const toggler = document.querySelector('.navbar-toggler');
+    const navbarContent = document.querySelector('#navbarSupportedContent');
+  
+    toggler.addEventListener('click', function() {
+      navbarContent.classList.toggle('show');
+    });
+  });
+  
